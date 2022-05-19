39f2a383e1acf53d31d2f2a9baba2b8b51c06298
cheblol <98470285+cheblol@users.noreply.github.com>
2022-05-19 07:50:26 +0600

    winning icons

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -47,16 +47,18 @@ class HealthIcon extends FlxSprite
 				name = 'icons/icon-face'; //Prevents crash from missing icon
 				if (!CharacterEditorState.inEditor)
 					FlxG.log.warn('Couldn\'t find icon file for $char!');
+				    FlxG.log.warn('Using the crash prevent icon, which is icon-face!');
 			}
 			var file = Paths.image(name);
 
 			loadGraphic(file); //Load stupidly first for getting the file size
-			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
+			loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr
 			iconOffsets[0] = (width - 150) / 2;
 			iconOffsets[1] = (width - 150) / 2;
+			iconOffsets[2] = (width - 150) / 2;
 			updateHitbox();
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 			if (char != 'bf-old') originalChar = char;
