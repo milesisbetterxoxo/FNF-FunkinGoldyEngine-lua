ad1dbca94539254f633b0bc90fa1575fbe10e50b
cheblol <98470285+cheblol@users.noreply.github.com>
2022-05-28 15:30:08 +0600

    create modchart functions epic lmao

---
+++ source/ModchartFunctions.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.FlxG;
+import openfl.*
+
+class ModchartFunctions {
+    public function resizeWindow(width, height)
+    {
+        resizeGame(width, height);
+    }
+    public function moveWindow(x, y)
+    {
+        Lib.application.window.move(x, y);
+    }
+}
