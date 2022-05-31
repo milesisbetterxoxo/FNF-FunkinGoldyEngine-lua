c14a2c7a468283d9e39b95a54177909f156511d3
cheb <98470285+cheblol@users.noreply.github.com>
2022-05-31 11:12:43 +0600

    bro 💀

--- source/ModchartFunctions.hx
+++ source/ModchartFunctions.hx
@@ -4,11 +4,11 @@ import flixel.FlxG;
 import openfl.*;
 
 class ModchartFunctions {
-    public function camZoom(width, height)
+    public static function camZoom(width:Int, height:Int)
     {
         FlxG.resizeGame(width, height);
     }
-    public function moveWindow(x, y)
+    public static function moveWindow(x:Int, y:Int)
     {
         Lib.application.window.move(x, y);
     }
