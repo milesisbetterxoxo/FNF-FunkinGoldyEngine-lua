0af97981465ffb8d15131ac5fdd2138f88418e40
cheb <98470285+cheblol@users.noreply.github.com>
2022-06-07 19:27:45 +0600

    stuff

---
+++ source/options/OptionsState.hx
@@ -0,0 +1,150 @@
+package options;
+
+#if DISCORD_ALLOWED
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+using StringTools;
+
+class OptionsState extends MusicBeatState
+{
+	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay', 'Save Data'];
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private static var curSelected:Int = 0;
+	public static var menuBG:FlxSprite;
+	static var goToPlayState:Bool = false;
+
+	public function new(?goToPlayState:Bool)
+	{
+		super();
+		if (goToPlayState != null)
+			OptionsState.goToPlayState = goToPlayState;
+	}
+
+	function openSelectedSubState(label:String) {
+		switch(label) {
+			case 'Note Colors':
+				openSubState(new options.NotesChooseSubState());
+			case 'Controls':
+				openSubState(new options.ControlsSubState());
+			case 'Graphics':
+				openSubState(new options.GraphicsSettingsSubState());
+			case 'Visuals and UI':
+				openSubState(new options.VisualsUISubState());
+			case 'Gameplay':
+				openSubState(new options.GameplaySettingsSubState());
+			case 'Adjust Delay and Combo':
+				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
+			case 'Save Data':
+				openSubState(new options.SaveDataSubState());
+		}
+	}
+
+	var selectorLeft:Alphabet;
+	var selectorRight:Alphabet;
+
+	override function create() {
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("Options Menu", null);
+		#end
+
+		FlxG.mouse.visible = true;
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...options.length)
+		{
+			var optionText:Alphabet = new Alphabet(0, 0, options[i], true, false);
+			optionText.screenCenter();
+			optionText.y += (100 * (i - (options.length / 2))) + 50;
+			grpOptions.add(optionText);
+		}
+
+		selectorLeft = new Alphabet(0, 0, '>', true, false);
+		add(selectorLeft);
+		selectorRight = new Alphabet(0, 0, '<', true, false);
+		add(selectorRight);
+
+		changeSelection();
+		ClientPrefs.saveSettings();
+
+		super.create();
+	}
+
+	override function closeSubState() {
+		super.closeSubState();
+		ClientPrefs.saveSettings();
+		for (sprite in members)
+		{
+			var sprite:Dynamic = sprite; //Make it check for FlxSprite instead of FlxBasic
+			var sprite:FlxSprite = sprite; //Don't judge me ok
+			if (sprite != null && (sprite is FlxSprite) && !(sprite is FlxText)) {
+				sprite.antialiasing = ClientPrefs.globalAntialiasing;
+			}
+		}
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		if (controls.UI_UP_P || FlxG.mouse.wheel > 0) {
+			changeSelection(-1);
+		}
+		if (controls.UI_DOWN_P || FlxG.mouse.wheel < 0) {
+			changeSelection(1);
+		}
+
+		if (controls.BACK) {
+			FlxG.mouse.visible = false;
+			FlxG.sound.play(Paths.sound('cancelMenu'), 0.7);
+			if (goToPlayState) {
+				StageData.loadDirectory(PlayState.SONG);
+				goToPlayState = false;
+				LoadingState.loadAndSwitchState(new PlayState(), true);
+			} else {
+				MusicBeatState.switchState(new MainMenuState());
+			}
+		}
+
+		if (controls.ACCEPT || FlxG.mouse.justPressed) {
+			openSelectedSubState(options[curSelected]);
+		}
+	}
+	
+	function changeSelection(change:Int = 0) {
+		curSelected += change;
+		if (curSelected < 0)
+			curSelected = options.length - 1;
+		if (curSelected >= options.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			item.alpha = 0.6;
+			if (item.targetY == 0) {
+				item.alpha = 1;
+				selectorLeft.x = item.x - 63;
+				selectorLeft.y = item.y;
+				selectorRight.x = item.x + item.width + 15;
+				selectorRight.y = item.y;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+	}
+}
