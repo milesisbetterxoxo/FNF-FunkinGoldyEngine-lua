a1c6dfee61baa44c672d9934c8f12f8d588f5456
cheb <98470285+cheblol@users.noreply.github.com>
2022-06-09 13:29:23 +0600

    huh

--- source/ModchartFunctions.hx
+++ source/ModchartFunctions.hx
@@ -16,19 +16,19 @@ class ModchartFunctions {
     public static function removeGroup(group:String)
     {
         if (group == 'gf' || group == 'girlfriend' || group == 'player3')
-        PlayState.instance.removeObject(PlayState.gf);
+        PlayState.instance.removeObject(PlayState.instance.gf);
         else if (group == 'bf' || group == 'boyfriend' || group == 'player2')
-        PlayState.instance.removeObject(PlayState.boyfriend);
+        PlayState.instance.removeObject(PlayState.instance.boyfriend);
         else if (group == 'dad' || group == 'player1')
-        PlayState.instance.removeObject(PlayState.dad);
+        PlayState.instance.removeObject(PlayState.instance.dad);
     }
     public static function addGroup(group:String)
     {
         if (group == 'gf' || group == 'girlfriend' || group == 'player3')
-        PlayState.instance.addObject(PlayState.gf);
+        PlayState.instance.addObject(PlayState.instance.gf);
         else if (group == 'bf' || group == 'boyfriend' || group == 'player2')
-        PlayState.instance.addObject(PlayState.boyfriend);
+        PlayState.instance.addObject(PlayState.instance.boyfriend);
         else if (group == 'dad' || group == 'player1')
-        PlayState.instance.addObject(PlayState.dad);
+        PlayState.instance.addObject(PlayState.instance.dad);
     }
 }
