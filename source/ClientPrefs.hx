d1c988c3200699d35388c910b9556a7da6bd5030
cheb <98470285+cheblol@users.noreply.github.com>
2022-06-09 11:13:03 +0600

    merging yeah

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -31,6 +31,7 @@ class ClientPrefs {
 	public static var underlayAlpha:Float = 0;
 	public static var instantRestart:Bool = false;
 	public static var flashingAmount:Float = 1;
+	public static var moreShit:String = 'Goldy'
 	#if !html5
 	public static var autoPause:Bool = true;
 	#else
@@ -247,6 +248,7 @@ class ClientPrefs {
 		FlxG.save.data.hitsoundVolume = hitsoundVolume;
 		FlxG.save.data.pauseMusic = pauseMusic;
 		FlxG.save.data.gameplaySettings = gameplaySettings;
+		FlxG.save.data.moreShit = moreShit;
 	
 		FlxG.save.flush();
 
