94c56e274d6870b3d02c36ff9d2d8f4f19a6c00c
cheb <98470285+cheblol@users.noreply.github.com>
2022-06-09 11:10:37 +0600

    addObject, removeObject from kade engine

--- source/ModchartFunctions.hx
+++ source/ModchartFunctions.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import openfl.*;
+import PlayState;
 
 class ModchartFunctions {
     public static function camZoom(width:Int, height:Int)
@@ -11,5 +12,23 @@ class ModchartFunctions {
     public static function moveWindow(x:Int, y:Int)
     {
         Lib.application.window.move(x, y);
+    }
+    public static function removeGroup(group:String)
+    {
+        if (group == 'gf' || group == 'girlfriend' || group == 'player3')
+        PlayState.instance.removeObject(PlayState.gf);
+        else if (group == 'bf' || group == 'boyfriend' || group == 'player2')
+        PlayState.instance.removeObject(PlayState.boyfriend);
+        else if (group == 'dad' || group == 'player1')
+        PlayState.instance.removeObject(PlayState.dad);
+    }
+    public static function addGroup(group:String)
+    {
+        if (group == 'gf' || group == 'girlfriend' || group == 'player3')
+        PlayState.instance.addObject(PlayState.gf);
+        else if (group == 'bf' || group == 'boyfriend' || group == 'player2')
+        PlayState.instance.addObject(PlayState.boyfriend);
+        else if (group == 'dad' || group == 'player1')
+        PlayState.instance.addObject(PlayState.dad);
     }
 }
