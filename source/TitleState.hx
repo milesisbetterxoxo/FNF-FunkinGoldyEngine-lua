ad292273062e5d3b7174ffca61900d0bd14802a7
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-07-21 13:27:46 -0400

    why logo no shader :(

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -274,7 +274,7 @@ class TitleState extends MusicBeatState
 		add(gfDance);
 		gfDance.shader = swagShader.shader;
 		add(logoBl);
-		//logoBl.shader = swagShader.shader;
+		logoBl.shader = swagShader.shader;
 
 		titleText = new FlxSprite(titleJSON.startx, titleJSON.starty);
 		titleText.frames = Paths.getSparrowAtlas('titleEnter');
