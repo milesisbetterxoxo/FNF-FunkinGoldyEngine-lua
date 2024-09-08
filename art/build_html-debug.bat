072ea25dc9c824cf3a0a7db2eb5b63c20fb738a2
miles <gamingchebyrek@gmail.com>
2024-09-08 09:06:02 +0100

    hmm

--- art/build_html-debug.bat
+++ art/build_html-debug.bat
@@ -2,6 +2,10 @@
 color 0a
 cd ..
 @echo on
+echo INSTALLING LIBRARIES
+haxelib --global install hmm
+haxelib --global run hmm setup
+hmm install
 echo BUILDING GAME
 lime test html5 -debug
 pause
--- art/build_x64-debug.bat
+++ art/build_x64-debug.bat
@@ -1,6 +1,10 @@
 @echo off
 color 0a
 cd ..
+echo INSTALLING LIBRARIES
+haxelib --global install hmm
+haxelib --global run hmm setup
+hmm install
 echo BUILDING GAME
 lime build windows -debug
 echo.
